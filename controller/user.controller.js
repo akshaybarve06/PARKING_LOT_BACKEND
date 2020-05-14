bbeb6eae1ac8eaa8a7fd22b4bfda768e6e4df20f
akshaybarve06 <akshaybarve06@gmail.com>
2020-05-14 20:29:29 +0530

    Added Controller To Redirect Routes To Model

---
+++ controller/user.controller.js
@@ -0,0 +1,22 @@
+const User = require('../app/models/user.model');
+
+// Create and Save a new User
+exports.register = (req, res) => {
+    // Validate request
+    if(!req.body.email) {
+        return res.status(400).send({
+            message: "email can not be empty"
+        });
+    }
+   // Create a New User
+    const user = new User({
+        name:req.body.name || "Unnamed User",
+        phone:req.body.phone, email:req.body.email, password:req.body.password
+    });
+    // Method To Save / Register User
+    User.register(user, req, res, result=>{
+        if(result){
+            return res.json({ message: result})
+        }
+    })
+};
