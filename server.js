0d4233754fb7881e785bb5a5ff6f2a60b83dd224
akshaybarve06 <akshaybarve06@gmail.com>
2020-05-14 20:13:01 +0530

    Added Code To Import Database Configuration To Server.js

--- server.js
+++ server.js
@@ -11,13 +11,29 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
 
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+})
+
 // define a simple route
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to Parking Lot API."});
 });
 
 // Require User routes
-require('./.vscode/routes/user.routes')(app);
+require('./routes/user.routes')(app);
 
 // listen for requests
 app.listen(port, () => {
